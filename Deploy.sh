d4f62917b2e1b007ef1b229f7be18abbb36d3d32
Richard-Su <suliquangm@gmail.com>
2021-01-18 17:19:26 +0800

    add project deploy script;

---
+++ Deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo docker stop violas-push_service
+sudo docker rm violas-push-service
+sudo docker image rm violas-push-service
+sudo docker image build --no-cache -t violas-push-service .
+sudo docker run --name=violas-push-service --network=host -d violas-push-service
