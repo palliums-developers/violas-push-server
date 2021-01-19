4fde53de33e270dfe8a06817b138acc4cf256d5e
Richard-Su <suliquangm@gmail.com>
2021-01-19 10:44:16 +0800

    change fcm private key file;

--- Common.py
+++ Common.py
@@ -1,4 +1,4 @@
-CERT_PATH = "./fcm-test-project-93d7f-firebase-adminsdk-1hb7h-11daf4862e.json"
+CERT_PATH = "./violas-push-server-firebase-adminsdk-62k74-bc2fb61931.json"
 
 TEST_DB_URL = f"postgres+psycopg2://postgres:@localhost:5432/violas_local"
 DB_URL = f"postgres+psycopg2://postgres:sMhu01zzb7XzB8inMcIH0Q==@localhost:6576/violas_local"
