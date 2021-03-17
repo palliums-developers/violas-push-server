7fbe8ad0f8a571e5167eafeab2ad9beb5d8c6d91
Richard-Su <suliquangm@gmail.com>
2021-03-17 17:01:45 +0800

    modify database connect config;

--- Common.py
+++ Common.py
@@ -1,4 +1,4 @@
 CERT_PATH = "./violas-push-server-firebase-adminsdk-62k74-bc2fb61931.json"
 
-TEST_DB_URL = f"postgres+psycopg2://postgres:@localhost:5432/violas_local"
-DB_URL = f"postgres+psycopg2://postgres:sMhu01zzb7XzB8inMcIH0Q==@localhost:6576/violas_local"
+TEST_DB_URL = f"postgresql+psycopg2://postgres:@localhost:5432/violas_local"
+DB_URL = f"postgresql+psycopg2://postgres:sMhu01zzb7XzB8inMcIH0Q==@localhost:6576/violas_local"
